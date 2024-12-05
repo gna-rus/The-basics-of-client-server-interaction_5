6f431aefb9c418d45fd79de3d84d2496b2ffc7c2
Nickolay Grigoriev <gnarus@inbox.ru>
2024-12-05 20:33:49 +0300

    first commit

---
+++ scrapy_project/runner.py
@@ -0,0 +1,16 @@
+# Это файл для удобного запуска пауков
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.reactor import install_reactor
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from scrapy_project.spiders.hhru import HhruSpider
+
+if __name__ == '__main__':
+    configure_logging()
+    settings = get_project_settings()
+    install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(HhruSpider)
+    process.start()
+
